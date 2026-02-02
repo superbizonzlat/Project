c70d95f280147d2e5e83b4578c67bfe476563102
superbisonzlat <andrey-osinkin@mail.ru>
2026-02-02 16:52:07 +0500

    ver 1.9.6

--- src/main/java/com/test/Project/repositories/BookingRepository.java
+++ src/main/java/com/test/Project/repositories/BookingRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Integer> {
-    //@Query("select u from Booking u left join fetch u.sportsField where :date between u.begin_at and u.end_at and u.sportsField.name = :name")
-    //Optional<Booking> findBookingByDate(@Param("date") LocalDateTime localDateTime, @Param("name") String s);
-    @Query("select u from Booking u  where :date between u.begin_at and u.end_at")
-    Optional<Booking> findBookingByDate(@Param("date") LocalDateTime localDateTime);
+    @Query("select u from Booking u left join fetch u.sportsField where :date between u.begin_at and u.end_at and u.sportsField.name = :name")
+    Optional<Booking> findBookingByDate(@Param("date") LocalDateTime localDateTime, @Param("name") String s);
+    //@Query("select u from Booking u  where :date between u.begin_at and u.end_at")
+    //Optional<List<Booking>> findBookingByDate(@Param("date") LocalDateTime localDateTime);
 }
